8160cfc34864a7c023ba69391173e1bbdd025469
Lynsey Kemplin <purplez479@gmail.com>
2022-10-24 18:35:00 -0700

    more algos

--- CodingDojoLearnPlatform/FundamentalsPt1.js
+++ CodingDojoLearnPlatform/FundamentalsPt1.js
@@ -12,3 +12,28 @@ function setAndSwitch() {
 }
 
 setAndSwitch();
+
+// Print -52 to 1066
+// Print integers from -52 to 1066 using a FOR loop.
+
+function printIntegers(){
+    for (i = -52; i < 1067; i++) {
+        console.log(i);
+    }
+}
+
+printIntegers();
+
+// Don’t Worry, Be Happy
+// Create beCheerful(). Within it, console.log string "good morning!" Call it 98 times.
+
+function beCheerful() {
+    let count = 0;
+    for (i=0; i<98; i++){
+        console.log("Good Morning!");
+        count +=1;
+    }
+    console.log(count);
+}
+
+beCheerful();
