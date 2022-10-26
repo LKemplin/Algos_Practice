01b73c1ffa67eb6e3a0e6d1be22eace098df90b4
Lynsey Kemplin <purplez479@gmail.com>
2022-10-25 17:46:42 -0700

    algos

--- CodingDojoLearnPlatform/FundamentalsPt1.js
+++ CodingDojoLearnPlatform/FundamentalsPt1.js
@@ -84,3 +84,21 @@ birthday(15, 01)
 birthday(22, 6)
 birthday(12, 25)
 birthday(1, 1)
+
+// Leap Year
+// Write a function that determines whether a given year is a leap year. If a year is divisible by four, it is a leap year, unless it is divisible by 100. However, if it is divisible by 400, then it is.
+
+function leapYear(year) {
+    if (year % 400 == 0) {
+        console.log(year + " is a leap year")
+    } else if (year % 4 == 0 && year % 100 !== 0) {
+        console.log(year + " is a leap year")
+    } else {
+        console.log(year + " is not a leap year")
+    }
+}
+
+leapYear(2000)
+leapYear(2001)
+leapYear(2004)
+leapYear(1900)
