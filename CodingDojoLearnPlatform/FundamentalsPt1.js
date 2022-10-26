eb9840fa05a441085c94c888e0df501d0c46f215
Lynsey Kemplin <purplez479@gmail.com>
2022-10-25 17:53:45 -0700

    more algo practice

--- CodingDojoLearnPlatform/FundamentalsPt1.js
+++ CodingDojoLearnPlatform/FundamentalsPt1.js
@@ -118,3 +118,19 @@ function printAndCount() {
 }
 
 printAndCount();
+
+// Multiples of Six
+// Print multiples of 6 up to 60,000, using a WHILE.
+
+function multiplesOfSix(i) {
+    while (i<= 60000) {
+        if (i % 6 == 0) {
+            console.log(i);
+            i++;
+        } else {
+            i++;
+        }
+    }
+}
+
+multiplesOfSix(0);
