03d9c770e8fb9b2fbf40fda64a19ce336e450814
Lynsey Kemplin <purplez479@gmail.com>
2022-10-25 18:07:52 -0700

    more algos

--- CodingDojoLearnPlatform/FundamentalsPt1.js
+++ CodingDojoLearnPlatform/FundamentalsPt1.js
@@ -151,3 +151,29 @@ function countingDojo() {
 }
 
 countingDojo();
+
+// What Do You Know?
+// Your function will be given an input parameter incoming. Please console.log this value.
+
+function incoming(value) {
+    console.log(value)
+}
+
+incoming("poop")
+incoming("hello")
+incoming(479)
+
+// Whoa, That Sucker’s Huge…
+// Add odd integers from -300,000 to 300,000, and console.log the final sum. Is there a shortcut?
+
+function addOdd() {
+    let sum = 0;
+    for (i=-300000; i<=300000; i++) {
+        if (i % 2 == 1) {
+            sum += i;
+        }
+    }
+    console.log(sum);
+}
+
+addOdd();
