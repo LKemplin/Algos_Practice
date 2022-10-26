20f98900652faf4ce30afb2be29d427dcaf1847e
Lynsey Kemplin <purplez479@gmail.com>
2022-10-25 18:28:39 -0700

    more algos

--- CodingDojoLearnPlatform/FundamentalsPt1.js
+++ CodingDojoLearnPlatform/FundamentalsPt1.js
@@ -177,3 +177,49 @@ function addOdd() {
 }
 
 addOdd();
+
+// Countdown by Fours
+// Log positive numbers starting at 2016, counting down by fours (exclude 0), without a FOR loop.
+
+function fourCountdown(i) {
+    while (i>0) {
+        if (i % 4 == 0) {
+            console.log(i);
+            i--;
+        } else {
+            i--;
+        }
+    }
+}
+
+fourCountdown(2016);
+
+// Flexible Countdown
+// Based on earlier “Countdown by Fours”, given lowNum, highNum, mult, print multiples of mult from highNum down to lowNum, using a FOR. For (2,9,3), print 9 6 3 (on successive lines).
+
+function flexibleCountdown(lowNum, highNum, mult) {
+    for (i=highNum; i >= lowNum; i--) {
+        if (i % mult == 0) {
+            console.log(i);
+        }
+    }
+}
+
+flexibleCountdown(6, 95, 5)
+
+// The Final Countdown
+// This is based on “Flexible Countdown”. The parameter names are not as helpful, but the problem is essentially identical; don’t be thrown off! Given 4 parameters (param1,param2,param3,param4), print the multiples of param1, starting at param2 and extending to param3. One exception: if a multiple is equal to param4, then skip (don’t print) it. Do this using a WHILE. Given (3,5,17,9), print 6,12,15 (which are all of the multiples of 3 between 5 and 17, and excluding the value 9).
+
+function finalCountdown(mult, range1, range2, nope) {
+    i = range1
+    while (i<=range2) {
+        if (i == nope {
+            i++;
+        } else if (i % mult == 0) {
+            console.log(i);
+            i++;
+            }
+        }
+    }
+
+finalCountdown(3, 5, 17, 9);
