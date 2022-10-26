966077cef0850cb8982c7c42ca305a043135ac02
Lynsey Kemplin <purplez479@gmail.com>
2022-10-25 17:50:57 -0700

    more algo practice

--- CodingDojoLearnPlatform/FundamentalsPt1.js
+++ CodingDojoLearnPlatform/FundamentalsPt1.js
@@ -102,3 +102,19 @@ leapYear(2000)
 leapYear(2001)
 leapYear(2004)
 leapYear(1900)
+
+// Print and Count
+// Print all integer multiples of 5, from 512 to 4096. Afterward, also log how many there were.
+
+function printAndCount() {
+    let count = 0;
+    for (i=512; i<=4096; i++) {
+        if (i % 5 == 0) {
+            console.log(i);
+            count++;
+        }
+    }
+    console.log(count);
+}
+
+printAndCount();
